72c35d40653eadb09b66c57e0476d0f59d9ac754
Catalin-code <catalin.despa.code@gmail.com>
2021-04-14 11:41:26 +0300

    Created and implemented controller, service, repo, config and model for video service

---
+++ video-service/src/main/java/com/codecool/videoservice/Video/VideoController.java
@@ -0,0 +1,25 @@
+package com.codecool.videoservice.Video;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class VideoController {
+
+    private final VideoService videoService;
+
+    @Autowired
+    public VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    @GetMapping("/videos")
+    public List<Video> getAllVideos(){
+        return videoService.getAllVideos();
+    }
+}
---
+++ video-service/src/main/java/com/codecool/videoservice/Video/VideoService.java
@@ -0,0 +1,21 @@
+package com.codecool.videoservice.Video;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VideoService {
+
+    private final VideoRepository videoRepository;
+
+    @Autowired
+    public VideoService(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    public List<Video> getAllVideos(){
+        return videoRepository.findAll();
+    }
+}
